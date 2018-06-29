772e89ecea325e3e6225f40b5acd5275fe464a6f
retrodaredevil <retrodaredevil@gmail.com>
2018-06-29 00:38:32 -0500

    Added demo screenshot to README and small changes to one file

--- core/src/me/retrodaredevil/game/trackshooter/entity/movement/AccelerationRotationalVelocityMoveComponent.java
+++ core/src/me/retrodaredevil/game/trackshooter/entity/movement/AccelerationRotationalVelocityMoveComponent.java
@@ -5,7 +5,8 @@ import me.retrodaredevil.game.trackshooter.util.RotationalVelocitySetter;
 public interface AccelerationRotationalVelocityMoveComponent extends RotationalVelocityMoveComponent, RotationalVelocitySetter {
 	/**
 	 * @param desiredRotationalVelocity The desired rotational velocity in degrees per second
-	 * @param rotationalAccelerationMultiplier The acceleration multiplier
+	 * @param rotationalAccelerationMultiplier The acceleration multiplier. If you want it to instantly accelerate, then it should be Float.POSITIVE_INFINITY.
+	 *                                         If you want it to fully accelerate in a number of seconds then is should be (1 / x seconds)
 	 * @param maxRotationalVelocity The maximum allows rotational velocity in degrees per second. If larger than desiredRotationalVelocity, has no effect
 	 */
 	@Override
